42fa5c886da6886fe7ea92be6cf8992cfb1803b4
ADDSynth <addsynth@gmail.com>
2024-08-17 21:07:38 -0500

    Null Analysis Update

--- src/main/java/addsynth/overpoweredmod/items/Ring.java
+++ src/main/java/addsynth/overpoweredmod/items/Ring.java
@@ -1,7 +1,6 @@
 package addsynth.overpoweredmod.items;
 
 import java.util.List;
-import javax.annotation.Nullable;
 import addsynth.core.compat.Compatibility;
 import addsynth.core.game.item.constants.ItemValue;
 import addsynth.overpoweredmod.compatability.curios.CuriosCapabilityProvider;
@@ -15,6 +14,7 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
