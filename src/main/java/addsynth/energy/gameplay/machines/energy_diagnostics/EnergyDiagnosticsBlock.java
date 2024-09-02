ad1e3ed5c1e4d3bb1c47d39d234845455051e487
ADDSynth <addsynth@gmail.com>
2024-09-02 11:08:30 -0500

    Added Energy Machine Tooltip to Energy Diagnostics

--- src/main/java/addsynth/energy/gameplay/machines/energy_diagnostics/EnergyDiagnosticsBlock.java
+++ src/main/java/addsynth/energy/gameplay/machines/energy_diagnostics/EnergyDiagnosticsBlock.java
@@ -1,14 +1,20 @@
 package addsynth.energy.gameplay.machines.energy_diagnostics;
 
+import java.util.List;
 import javax.annotation.Nullable;
 import addsynth.core.game.blocks.TileEntityBlock;
 import addsynth.core.util.game.MinecraftUtility;
 import addsynth.energy.gameplay.client.GuiProvider;
+import addsynth.energy.gameplay.reference.EnergyText;
 import addsynth.energy.registers.Tiles;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -22,6 +28,11 @@ public final class EnergyDiagnosticsBlock extends TileEntityBlock {
 
   public EnergyDiagnosticsBlock(){
     super(Properties.of(Material.METAL, MaterialColor.WOOL).strength(1.5f, 6.0f));
+  }
+
+  @Override
+  public final void appendHoverText(ItemStack stack, @Nullable BlockGetter worldIn, List<Component> tooltip, TooltipFlag flagIn){
+    tooltip.add(EnergyText.energy_machine);
   }
 
   @Override
